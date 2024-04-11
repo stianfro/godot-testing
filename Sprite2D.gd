92227b706f9779942ce10faf437448cdbd1d4ff9
Stian Frøystein <stianfroy@gmail.com>
2024-04-11 09:12:51 +0900

    feat: add speedup input

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -1,9 +1,12 @@
 extends Sprite2D
 
 func _process(delta):
-	var speed = 100
+	var speed = 800
 	var move = Vector2()
-	
+
+	if Input.is_action_pressed("ui_accept"):
+		speed = 2000
+
 	if Input.is_action_pressed("ui_right"):
 		move.x += 1
 	if Input.is_action_pressed("ui_left"):
@@ -12,6 +15,6 @@ func _process(delta):
 		move.y += 1
 	if Input.is_action_pressed("ui_up"):
 		move.y -= 1
-	
+
 	move = move.normalized() * speed * delta
 	position += move
